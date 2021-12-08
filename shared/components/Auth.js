a97aacb907bf58475d29fb72426900d2cee34052
colorlessenergy <brianmunozdev@gmail.com>
2021-12-07 19:25:11 -0800

    add close modal button

--- shared/components/Auth.js
+++ shared/components/Auth.js
@@ -55,6 +55,17 @@ const Auth = ({ toggleAuthModal }) => {
 
     return (
         <div>
+            <button
+                onClick={ toggleAuthModal }
+                className="modal-close-button"
+                title="close">
+                <svg
+                    className="modal-close-button__icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
             <form onSubmit={ handleSubmit }>
                 <div className="auth-form-group">
                     <label htmlFor="email">email</label>
