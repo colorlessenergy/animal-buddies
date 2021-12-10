9d63861f3c666f980c2fe7a1662748a692d4d745
colorlessenergy <brianmunozdev@gmail.com>
2021-12-09 18:13:15 -0800

    add header text to auth modal

--- shared/components/Auth.js
+++ shared/components/Auth.js
@@ -66,6 +66,14 @@ const Auth = ({ toggleAuthModal }) => {
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </button>
+
+            <div className="text-2 font-weight-700 text-center">
+                animal buddies
+            </div>
+            <p className="text-center">
+                { isSignInView ? ("log in to like posts") : ("create an account to like posts") }
+            </p>
+
             <form onSubmit={ handleSubmit }>
                 <div className="auth-form-group">
                     <label htmlFor="email">email</label>
